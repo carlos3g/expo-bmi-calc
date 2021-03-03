5087162b723b8c0de626b2bc4e947754e6ea7625
carlos3g <carlosmesquita156@gmail.com>
2021-03-03 17:43:08 -0300

    [REFACTOR] change wrapper color

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Wrapper = styled(SafeAreaView)`
-  background-color: #282a36;
+  background-color: #0A0F32;
   justify-content: center;
   align-items: center;
   flex: 1;
