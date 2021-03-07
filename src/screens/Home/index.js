ddb446bb0889e67398992b353af00de9cbdf1773
carlos3g <carlosmesquita156@gmail.com>
2021-03-07 19:44:33 -0300

    [FEAT] creates Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Card from '@components/Card';
+import Modifier from '@components/Modifier';
 
 import {
   Wrapper,
+  Container,
+  Section,
+  Label,
+  GenderMaleIcon,
+  GenderFemaleIcon,
+  H1,
+  H6,
+  Range,
+  ModifiersContainer,
   CalculateButton,
   CalculateButtonText,
-  Container,
 } from './styles';
 
 function HomeScreen() {
+  const [gender, setGender] = useState(0); // {0: "Male", 1: "Female"}
+  const [height, setHeight] = useState(100);
+  const [weight, setWeight] = useState(50);
+  const [age, setAge] = useState(10);
+
+  function handleAddModifier(value, func) {
+    func(value + 1);
+  }
+  function handleMinusModifier(value, func) {
+    func(value - 1);
+  }
+
   return (
     <Wrapper>
-      <Container></Container>
+      <Container>
+        <Section horizontal>
+          <Card onPress={() => setGender(0)}>
+            <GenderMaleIcon focus={gender == 0} />
+            <Label focus={gender == 0}>Male</Label>
+          </Card>
+          <Card onPress={() => setGender(1)}>
+            <GenderFemaleIcon focus={gender} />
+            <Label focus={gender}>Female</Label>
+          </Card>
+        </Section>
+
+        <Section>
+          <Card full>
+            <Label>Height</Label>
+            <H1>
+              {height}
+              <H6>cm</H6>
+            </H1>
+            <Range
+              onValueChange={(value) => setHeight(value)}
+              step={1}
+              minimumValue={100}
+              maximumValue={300}
+            />
+          </Card>
+        </Section>
+
+        <Section horizontal>
+          <Card>
+            <Label>Weight</Label>
+            <H1>{weight}</H1>
+            <ModifiersContainer>
+              <Modifier
+                type="plus"
+                onPress={() => handleAddModifier(weight, setWeight)}
+              />
+              <Modifier
+                type="minus"
+                onPress={() => handleMinusModifier(weight, setWeight)}
+              />
+            </ModifiersContainer>
+          </Card>
+
+          <Card>
+            <Label>Age</Label>
+            <H1>{age}</H1>
+            <ModifiersContainer>
+              <Modifier
+                type="plus"
+                onPress={() => handleAddModifier(age, setAge)}
+              />
+              <Modifier
+                type="minus"
+                onPress={() => handleMinusModifier(age, setAge)}
+              />
+            </ModifiersContainer>
+          </Card>
+        </Section>
+      </Container>
 
       <CalculateButton>
         <CalculateButtonText>Calculate your BMI</CalculateButtonText>
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -4,39 +4,48 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
+import Slider from '@react-native-community/slider';
+
 const SectionHorizontalStyles = `\
 flex-direction: row;
 justify-content: space-between
 `;
 
 export const Wrapper = styled(SafeAreaView)`
-  background-color: #0a0f32;
+  background-color: #0a0f33;
   flex: 1;
 `;
 
-export const Container = styled.ScrollView`
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 20,
+  },
+})`
   flex: 1;
-  padding: 20px 20px 0 20px;
-  margin-bottom: 20px;
 `;
 
 export const Section = styled.View`
+  margin-bottom: 20px;
   ${({ horizontal }) => horizontal && SectionHorizontalStyles}
 `;
 
-export const GenderMaleIcon = styled(MaterialCommunityIcons).attrs({
-  name: 'gender-male',
-  size: 80,
-  color: '#f8f8f2',
-})`
+export const GenderMaleIcon = styled(MaterialCommunityIcons).attrs(
+  ({ focus }) => ({
+    name: 'gender-male',
+    size: 80,
+    color: focus ? '#f8f8f2' : '#A3A6B5',
+  })
+)`
   margin-bottom: 20px;
 `;
 
-export const GenderFemaleIcon = styled(MaterialCommunityIcons).attrs({
-  name: 'gender-female',
-  size: 80,
-  color: '#f8f8f2',
-})`
+export const GenderFemaleIcon = styled(MaterialCommunityIcons).attrs(
+  ({ focus }) => ({
+    name: 'gender-female',
+    size: 80,
+    color: focus ? '#f8f8f2' : '#A3A6B5',
+  })
+)`
   margin-bottom: 20px;
 `;
 
@@ -46,7 +55,7 @@ export const CalculateButton = styled.TouchableOpacity`
   align-items: center;
 
   width: 100%;
-  height: 75px;
+  height: 68px;
 `;
 
 export const CalculateButtonText = styled.Text`
@@ -54,4 +63,38 @@ export const CalculateButtonText = styled.Text`
   font-weight: bold;
   font-size: 20px;
   color: #f8f8f2;
+`;
+
+export const Label = styled.Text`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 16px;
+  color: ${({ focus }) => (focus ? '#f8f8f2' : '#A3A6B5')};
+`;
+
+export const Range = styled(Slider).attrs({
+  thumbTintColor: '#e6005c',
+  minimumTrackTintColor: '#f8f8f2',
+  maximumTrackTintColor: '#a3a6b5',
+})`
+  width: 100%;
+  margin: 20px 0 20px 0;
+`;
+
+export const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+  color: #f8f8f2;
+`;
+
+export const H6 = styled.Text`
+  font-size: 16px;
+  color: #a3a6b5;
+`;
+
+export const ModifiersContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 16px;
+  width: 100%;
 `;
