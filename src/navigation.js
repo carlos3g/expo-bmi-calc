2bff16c6e815da51b63e2033049dc13a973317be
carlos3g <carlosmesquita156@gmail.com>
2021-03-08 16:29:14 -0300

    [REFACTOR] change transition animation

--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs } from '@react-navigation/stack';
 
 import ResultScreen from '@screens/Result';
 import HomeScreen from '@screens/Home';
@@ -13,7 +13,15 @@ const screenOptions = {
 
 function Routes() {
   return (
-    <Navigator screenOptions={screenOptions}>
+    <Navigator
+      screenOptions={screenOptions}
+      options={{
+        transitionSpec: {
+          open: TransitionSpecs.RevealFromBottomAndroidSpec,
+          close: TransitionSpecs.RevealFromBottomAndroidSpec,
+        },
+      }}
+    >
       <Screen name="Home" component={HomeScreen} />
       <Screen name="Result" component={ResultScreen} />
     </Navigator>
