0608741ec1ea4063f80b0cbd5f1f18827e868ef5
carlos3g <carlosmesquita156@gmail.com>
2021-03-09 13:28:25 -0300

    [FEAT] creates styles folder with colors.js file

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  primaryDark: '#0a0f33',
+  primary: '#13183a',
+  primaryLight: '#222747',
+  secondary: '#e6005c',
+  active: '#f8f8f2',
+  inactive: '#A3A6B5',
+  green: '#39ff14',
+  white: '#f8f8f2',
+  gray: '#A3A6B5',
+};
+
+export default colors;
