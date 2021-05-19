3e705c3c6db207f971ab7f824dbb28501e68024d
carlos3g <carlosmesquita156@gmail.com>
2021-05-19 19:00:58 -0300

    [REFACTOR] remove expo-status-bar package

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 
 import Routes from './navigation';
 
 function App() {
   return (
     <>
-      <StatusBar style="light" backgroundColor="#0A0F32" />
       <NavigationContainer>
         <Routes />
       </NavigationContainer>
